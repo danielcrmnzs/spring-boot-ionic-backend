39d198086638a64fec646b04af5f4986fa6a3b06
Daniel Coimbra <danielcrmnzs@hotmail.com>
2021-11-06 09:41:47 -0300

    Projeto criado

---
+++ src/test/java/com/devcoi/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcoi.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
