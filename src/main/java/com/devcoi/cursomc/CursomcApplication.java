36c8c795ff5c0cd99f12e445e5cd7893998a5ba6
Daniel Coimbra <danielcrmnzs@hotmail.com>
2022-01-13 18:14:26 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/devcoi/cursomc/CursomcApplication.java
+++ src/main/java/com/devcoi/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.devcoi.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.devcoi.cursomc.domain.Cidade;
 import com.devcoi.cursomc.domain.Cliente;
 import com.devcoi.cursomc.domain.Endereco;
 import com.devcoi.cursomc.domain.Estado;
+import com.devcoi.cursomc.domain.Pagamento;
+import com.devcoi.cursomc.domain.PagamentoComBoleto;
+import com.devcoi.cursomc.domain.PagamentoComCartao;
+import com.devcoi.cursomc.domain.Pedido;
 import com.devcoi.cursomc.domain.Produto;
+import com.devcoi.cursomc.domain.enums.EstadoPagamento;
 import com.devcoi.cursomc.domain.enums.TipoCliente;
 import com.devcoi.cursomc.repositories.CategoriaRepository;
 import com.devcoi.cursomc.repositories.CidadeRepository;
 import com.devcoi.cursomc.repositories.ClienteRepository;
 import com.devcoi.cursomc.repositories.EnderecoRepository;
 import com.devcoi.cursomc.repositories.EstadoRepository;
+import com.devcoi.cursomc.repositories.PagamentoRepository;
+import com.devcoi.cursomc.repositories.PedidoRepository;
 import com.devcoi.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -35,63 +43,85 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
-		
+
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
-				
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		
+
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
-		
+
 		Cidade c1 = new Cidade(null, "Uberlândia", est1);
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
-		
+
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
-		
+
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-		
+
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "33848965728", TipoCliente.PESSOA_FISICA);
-		
+
 		cli1.getTelefones().addAll(Arrays.asList("978956412", "98657854"));
-		
-		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 201", "Jardim", "33654789", cli1, c1); 
-		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "32685289", cli1, c2); 
-		
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 201", "Jardim", "33654789", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "32685289", cli1, c2);
+
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
-		
+
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
-		
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+
+		Pedido pedi1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido pedi2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, pedi1, 6);
+		pedi1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, pedi2, sdf.parse("20/10/2017 00:00"),
+				null);
+		pedi2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(pedi1, pedi2));
+
+		pedidoRepository.saveAll(Arrays.asList(pedi1, pedi2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
 	}
 
 }
