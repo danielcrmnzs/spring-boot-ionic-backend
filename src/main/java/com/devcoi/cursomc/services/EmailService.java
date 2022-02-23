97a0c349539512432073dfcadd46a3ddf70de84c
Daniel Coimbra <danielcrmnzs@hotmail.com>
2022-02-23 19:11:42 -0300

    Email HTML

--- src/main/java/com/devcoi/cursomc/services/EmailService.java
+++ src/main/java/com/devcoi/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.devcoi.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.devcoi.cursomc.domain.Pedido;
@@ -7,7 +9,11 @@ import com.devcoi.cursomc.domain.Pedido;
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
-	
+
 	void sendEmail(SimpleMailMessage msg);
-	
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
+
 }
--- src/main/java/com/devcoi/cursomc/services/PedidoService.java
+++ src/main/java/com/devcoi/cursomc/services/PedidoService.java
@@ -34,10 +34,10 @@ public class PedidoService {
 
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@Autowired
 	private ProdutoService produtoService;
-	
+
 	@Autowired
 	private EmailService emailService;
 
@@ -67,9 +67,7 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
-		
-		emailService.sendOrderConfirmationEmail(obj);
-		
+		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
 
