b8a1e48989154b093fc79d718633af272c8fc639
Daniel Coimbra <danielcrmnzs@hotmail.com>
2022-03-15 22:07:42 -0300

    Esqueci a senha

---
+++ src/main/java/com/devcoi/cursomc/services/AuthService.java
@@ -0,0 +1,62 @@
+package com.devcoi.cursomc.services;
+
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.devcoi.cursomc.domain.Cliente;
+import com.devcoi.cursomc.repositories.ClienteRepository;
+import com.devcoi.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class AuthService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@Autowired
+	private EmailService emailService;
+
+	private Random random = new Random();
+
+	public void sendNewPassword(String email) {
+		Cliente cliente = clienteRepository.findByEmail(email);
+		if (cliente == null) {
+			throw new ObjectNotFoundException("Email não encontrado");
+		}
+
+		String newPassword = newPassword();
+		cliente.setSenha(bCryptPasswordEncoder.encode(newPassword));
+
+		clienteRepository.save(cliente);
+		emailService.sendNewPasswordEmail(cliente, newPassword);
+	}
+
+	private String newPassword() {
+		char[] vet = new char[10];
+		for (int i = 0; i < 10; i++) {
+			vet[i] = randomChar();
+		}
+		return new String(vet);
+	}
+
+	private char randomChar() {
+		int opt = random.nextInt(3);
+		if (opt == 0) {
+			// gerar um digito
+			return (char) (random.nextInt(10) + 48);
+		} else if (opt == 1) {
+			// gerar uma letra maiúscula
+			return (char) (random.nextInt(26) + 65);
+		} else {
+			// gerar uma letra minúscula
+			return (char) (random.nextInt(26) + 97);
+		}
+	}
+
+}
