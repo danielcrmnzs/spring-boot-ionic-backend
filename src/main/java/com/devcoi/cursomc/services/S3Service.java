294431950272dc8d8291423bf4ea82cdbb6f2ac7
Daniel Coimbra <danielcrmnzs@hotmail.com>
2022-03-26 11:31:25 -0300

    Enviando imagem via endpoint

--- src/main/java/com/devcoi/cursomc/services/S3Service.java
+++ src/main/java/com/devcoi/cursomc/services/S3Service.java
@@ -1,16 +1,22 @@
 package com.devcoi.cursomc.services;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.devcoi.cursomc.utils.FileUtils;
 
@@ -39,4 +45,27 @@ public class S3Service {
 		}
 	}
 
+	public URI uploadFile(MultipartFile multipartFile) {
+		try {
+			String fileName = multipartFile.getOriginalFilename();
+			InputStream input = multipartFile.getInputStream();
+			String contentType = multipartFile.getContentType();
+			return uploadFile(input, fileName, contentType);
+		} catch (IOException e) {
+			throw new RuntimeException("Erro de IO: " + e.getMessage());
+		}
+	}
+
+	public URI uploadFile(InputStream input, String fileName, String contentType) {
+		try {
+			ObjectMetadata meta = new ObjectMetadata();
+			meta.setContentType(contentType);
+			LOG.info("Iniciando upload...");
+			s3client.putObject(new PutObjectRequest(bucketName, fileName, input, meta));
+			LOG.info("Upload finalizado.");
+			return s3client.getUrl(bucketName, fileName).toURI();
+		} catch (URISyntaxException e) {
+			throw new RuntimeException("Erro ao tentar converter URL para URI");
+		}
+	}
 }
